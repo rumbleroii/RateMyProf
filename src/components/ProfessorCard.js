1003646798746b2d8bbf67fd41a16e65fb0448f5
Mubashir <mubashir1728@gmail.com>
2023-10-11 21:37:33 +0530

    commit

--- src/components/ProfessorCard.js
+++ src/components/ProfessorCard.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './ProfessorCard.css'; // Import the CSS file for styling
-
+import './ProfessorCard.css'; 
 function ProfessorCard({ name, department, email, phoneNumber }) {
   return (
     <div className="professor-card">
