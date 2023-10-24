2a1f3d611bc19ac2f2bbd37837f3f4df1c5d8adb
Jytesh <44925963+Jytesh@users.noreply.github.com>
2023-10-24 22:39:44 +0530

    Changed config
    Signed-off-by: Jytesh <44925963+Jytesh@users.noreply.github.com>

--- src/components/firebaseconfig.js
+++ src/components/firebaseconfig.js
@@ -1,16 +1,14 @@
-const fb = {
-  apiKey: "AIzaSyCLiChKAHpDscte8udUa8z2I_uZlbK5zsY",
-  authDomain: "ratemyprof-584f8.firebaseapp.com",
-  databaseURL:
-    "https://ratemyprof-584f8-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "ratemyprof-584f8",
-  storageBucket: "ratemyprof-584f8.appspot.com",
-  messagingSenderId: "1017467728771",
-  appId: "1:1017467728771:web:0a9cb5f7f2f5f4e63cfcb6",
-  measurementId: "G-XVJ1Q67L0D",
+const firebaseConfig = {
+  apiKey: "AIzaSyBdLA0IXhD0oZEvrgEMkWYa63jmoL4XlkQ",
+  authDomain: "ratemyprofessor-de921.firebaseapp.com",
+  projectId: "ratemyprofessor-de921",
+  storageBucket: "ratemyprofessor-de921.appspot.com",
+  messagingSenderId: "435675584965",
+  appId: "1:435675584965:web:1249c5335eb5f7de48d3ac",
+  measurementId: "G-3CBZ6HBG6X",
 };
 
-const firebaseConfig = {
+const fb = {
   apiKey: "AIzaSyAIc4h-T2yDWjhOmlyPSBf69GV8CwaRLDY",
   authDomain: "ig-rmp.firebaseapp.com",
   projectId: "ig-rmp",
